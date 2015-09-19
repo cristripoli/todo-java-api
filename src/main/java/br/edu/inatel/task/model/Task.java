a71b46b88f137efd7cc2aeeb04d45f35a8123987
Crislaine <crislaine@inatel.br>
2015-09-19 12:40:55 -0300

    First commit

--- src/main/java/br/edu/inatel/task/model/Task.java
+++ src/main/java/br/edu/inatel/task/model/Task.java
@@ -4,4 +4,38 @@ package br.edu.inatel.task.model;
  * Created by crislaine on 19/09/2015.
  */
 public class Task {
+    private Long id;
+    private Boolean done;
+    private String description;
+
+    public Task(){
+    }
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
